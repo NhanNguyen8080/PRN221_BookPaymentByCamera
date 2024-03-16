f5f6ec6730478fd4e2571b31a9c0347258dcfc71
NhanNguyen200 <nhannq@hisoft.vn>
2024-03-16 13:35:46 +0700

    Build project

---
+++ BookPaymentByCamera/ImageModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace BookPaymentByCamera
+{
+    public class ImageModel
+    {
+        public int Id { get; set; }
+        public BitmapSource Image { get; set; }
+
+        //public override bool Equals(object obj)
+        //{
+        //    if (obj == null || GetType() != obj.GetType())
+        //        return false;
+
+        //    ImageModel other = (ImageModel)obj;
+
+        //    // Implement your equality comparison based on the properties of ImageModel
+        //    return this.Id == other.Id && this.FileName == other.FileName;
+        //}
+
+        // You should also override GetHashCode when overriding Equals
+        //public override int GetHashCode()
+        //{
+        //    // Implement a hash code based on the same properties used in Equals
+        //    return HashCode.Combine(Id, FileName);
+        //}
+
+    }
+}
---
+++ BookPaymentByCamera/MainWindow.xaml.cs
@@ -0,0 +1,311 @@
+﻿using AForge.Video;
+using AForge.Video.DirectShow;
+using IronOcr;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Text;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace BookPaymentByCamera
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private FilterInfoCollection videoDevices;
+        private VideoCaptureDevice videoSource;
+        private System.Timers.Timer captureTimer;
+        private System.Timers.Timer stopTimer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            btnCapture.IsEnabled = false;
+            lvImage.ItemsSource = null;
+            List<ImageModel> list = GetImageModels("D:\\StudyDocuments\\PRN221\\GroupProject\\BookPaymentByCamera\\BookPaymentByCamera\\CapturedImages");
+            lvImage.ItemsSource = list;
+        }
+
+        private void btnStartStop_Click(object sender, RoutedEventArgs e)
+        {
+            if (btnStartStop.Content.Equals("Start"))
+            {
+                videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+                if (videoDevices.Count > 0)
+                {
+                    videoSource = new VideoCaptureDevice(videoDevices[0].MonikerString);
+                    videoSource.NewFrame += VideoSource_NewFrame;
+                    videoSource.Start();
+                }
+                else
+                {
+                    System.Windows.MessageBox.Show("Notification", "No video devices found!", MessageBoxButton.OK);
+                }
+                btnStartStop.Content = "Stop";
+                btnCapture.IsEnabled = true;
+            }
+
+            else
+            {
+                if (videoSource != null && videoSource.IsRunning)
+                {
+                    videoSource.SignalToStop();
+                    imgWebcam.Source = null;
+                }
+                btnStartStop.Content = "Start";
+                btnCapture.IsEnabled = false;
+                //captureTimer.Stop();
+                //captureTimer.Dispose();
+            }
+            //InitializeTimer();
+
+        }
+
+        private void VideoSource_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+
+            imgWebcam.Dispatcher.Invoke(() =>
+            {
+                imgWebcam.Source = ToBitmapImage(eventArgs.Frame);
+            });
+
+
+        }
+
+        private BitmapImage ToBitmapImage(Bitmap bitmap)
+        {
+            using (MemoryStream memory = new MemoryStream())
+            {
+                bitmap.Save(memory, ImageFormat.Bmp);
+                memory.Position = 0;
+
+                BitmapImage bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memory;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+
+                return bitmapImage;
+            }
+        }
+
+        private List<ImageModel> GetImageModels(string folderPath)
+        {
+            List<ImageModel> imageModels = new List<ImageModel>();
+
+            if (Directory.Exists(folderPath))
+            {
+                var files = Directory.GetFiles(folderPath);
+                int id = 1;
+                foreach (var file in files)
+                {
+                    var lastIndexOfPoint = file.LastIndexOf(".");
+                    var extension = file.Substring(lastIndexOfPoint);
+                    if (extension.Equals(".png") || extension.Equals(".jpg") || extension.Equals(".jpeg"))
+                    {
+                        Bitmap imageBitmap = new Bitmap(file);
+                        try
+                        {
+                            var imageSource = ToBitmapImage(imageBitmap);
+                            imageModels.Add(new ImageModel { Id = id, Image = imageSource });
+                            id++;
+                        }
+                        finally
+                        {
+                            imageBitmap.Dispose();
+                        }
+                    }
+
+                }
+                return imageModels.ToList();
+            }
+
+            return null;
+            
+        }
+        //private void InitializeTimer()
+        //{
+        //    // Set the capture interval to 1 second (1000 milliseconds)
+        //    int captureInterval = 1000 / 60;
+
+        //    captureTimer = new System.Timers.Timer(captureInterval);
+        //    //captureTimer.Interval = TimeSpan.FromMilliseconds(captureInterval);
+        //    captureTimer.Elapsed += Capture;
+        //    captureTimer.Start();
+
+        //    // Set the stop interval to 1 second (1000 milliseconds)
+        //    int stopInterval = 1000;
+
+        //    stopTimer = new System.Timers.Timer(stopInterval);
+        //    stopTimer.Elapsed += StopCaptureTimer;
+        //    stopTimer.Start();
+        //}
+
+        //private void StopCaptureTimer(object sender, ElapsedEventArgs e)
+        //{
+        //    // Stop the captureTimer after 1 second
+        //    captureTimer.Stop();
+        //    stopTimer.Stop();
+        //    stopTimer.Dispose();
+        //}
+
+
+        private void Capture(object sender, ElapsedEventArgs e)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                if (videoSource.IsRunning && videoSource != null)
+                {
+                    Thread.Sleep(1000);
+                    if (imgWebcam.Source != null && imgWebcam.Source is BitmapSource bitmapSource)
+                    {
+
+                        var encoder = new PngBitmapEncoder();
+                        encoder.Frames.Add(BitmapFrame.Create(bitmapSource));
+
+                        var randomString = Guid.NewGuid().ToString().Substring(0, 32);
+                        var fileName = $"{randomString}_{DateTime.Now:yyyyMMddHHmmSS}.png";
+                        string workingDirectory = Environment.CurrentDirectory;
+                        var rootFolderPath = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
+                        var folderPath = System.IO.Path.Combine(rootFolderPath, "CapturedImages");
+                        var filePath = System.IO.Path.Combine(folderPath, fileName);
+
+                        if (!Directory.Exists(folderPath))
+                        {
+                            Directory.CreateDirectory(folderPath);
+                        }
+
+                        using (var fileStream = new FileStream(filePath, FileMode.Create))
+                        {
+                            encoder.Save(fileStream);
+                        }
+
+                        //System.Windows.MessageBox.Show($"Image saved to: {filePath}");
+
+                        //lvImage.ItemsSource = null;
+                        //List<ImageModel> list = GetImageModels(txtBrowse.Text);
+                        //lvImage.ItemsSource = list;
+                    }
+
+                    else
+                    {
+                        System.Windows.MessageBox.Show($"No image to save!");
+                    }
+                }
+                else
+                {
+                    System.Windows.MessageBox.Show($"Your webcam is closing!");
+                }
+            });
+        }
+
+        private void btnCapture_Click(object sender, EventArgs e)
+        {
+
+            if (videoSource.IsRunning && videoSource != null)
+            {
+                if (imgWebcam.Source != null && imgWebcam.Source is BitmapSource bitmapSource)
+                {
+
+                    var encoder = new PngBitmapEncoder();
+                    encoder.Frames.Add(BitmapFrame.Create(bitmapSource));
+
+                    var randomString = Guid.NewGuid().ToString().Substring(0, 32);
+                    var fileName = $"{randomString}_{DateTime.Now:yyyyMMddHHmmSS}.png";
+                    string workingDirectory = Environment.CurrentDirectory;
+                    var rootFolderPath = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
+                    var folderPath = System.IO.Path.Combine(rootFolderPath, "CapturedImages");
+                    var filePath = System.IO.Path.Combine(folderPath, fileName);
+
+                    if (!Directory.Exists(folderPath))
+                    {
+                        Directory.CreateDirectory(folderPath);
+                    }
+
+                    using (var fileStream = new FileStream(filePath, FileMode.Create))
+                    {
+                        encoder.Save(fileStream);
+                    }
+
+
+                    lvImage.ItemsSource = null;
+                    List<ImageModel> list = GetImageModels(folderPath);
+                    lvImage.ItemsSource = list;
+
+                    var ocrList = OcrScan(filePath);
+
+                }
+
+                else
+                {
+                    System.Windows.MessageBox.Show($"No image to save!");
+                }
+            }
+            else
+            {
+                System.Windows.MessageBox.Show($"Your webcam is closing!");
+            }
+
+        }
+
+        private List<string> OcrScan(string imageUrl)
+        {
+            var ironOcr = new IronTesseract();
+            var image = new System.Drawing.Bitmap(imageUrl);
+            var result = ironOcr.Read(image);
+            Paragraph paragraph = new Paragraph(new Run(result.Text));
+            //rtxtOcr.Document.Blocks.Add(paragraph);
+
+            string[] lines = result.Text.Split(new char[] { '\n' });
+            List<string> resultList = new List<string>(lines);
+            return resultList;
+        }
+
+        private void btnPayment_Click(object sender, RoutedEventArgs e)
+        {
+            var folderPath = "D:\\StudyDocuments\\PRN221\\GroupProject\\BookPaymentByCamera\\BookPaymentByCamera\\CapturedImages";
+            try
+            {
+                // Check if the folder exists
+                if (Directory.Exists(folderPath))
+                {
+                    // Get all files in the folder
+                    string[] files = Directory.GetFiles(folderPath);
+
+                    // Iterate through each file and delete it
+                    foreach (string file in files)
+                    {
+                        File.Delete(file);
+                    }
+
+                    System.Windows.MessageBox.Show("Files removed successfully.", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                else
+                {
+                    System.Windows.MessageBox.Show("Folder does not exist.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                lvImage.ItemsSource = null;
+                List<ImageModel> list = GetImageModels(folderPath);
+                lvImage.ItemsSource = list;
+
+            }
+            catch (Exception ex)
+            {
+                System.Windows.MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
